285bb72e93974fc8cbf494122692f84ca9c0a613
roopemyller <roope.myller@gmail.com>
2026-04-05 08:06:46 +0300

    Map now uses MapPinMarker component as mappins

--- Frontend/app/routes/home.tsx
+++ Frontend/app/routes/home.tsx
@@ -1,29 +1,42 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-import { Link } from "react-router";
 
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, useMapEvent } from "react-leaflet";
 import { Marker, Popup } from "react-leaflet";
+import { useRef } from "react";
 
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+import Button from "~/components/Button";
+import MapPinMarker from "~/components/MapPin";
+
+function SetViewOnClick({ animateRef }: { animateRef: React.MutableRefObject<boolean> }) {
+  const map = useMapEvent("click", (e) => {
+    map.setView(e.latlng, map.getZoom(), { animate: animateRef.current || false });
+  }
+  );
+  return null;
 }
 
 export default function Home() {
+
+  const animateRef = useRef(true);
+
+  const places = [
+    { id: 1, type: "restaurant", position: [61.05692, 28.19061], name: "Bistro" },
+    { id: 2, type: "pizza", position: [61.0574, 28.192], name: "Pizza Spot" },
+    { id: 3, type: "vegan", position: [61.0558, 28.1892], name: "Green Bowl" },
+  ] as const;
+
   return (
-    <MapContainer center={[61.05692, 28.19061]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[61.05692, 28.19061]} zoom={15} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[61.05692, 28.19061]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+      <SetViewOnClick animateRef={animateRef} />
+
+      {places.map((p) => (
+        <MapPinMarker key={p.id} position={[p.position[0], p.position[1]]} type={p.type} size={40} popup={p.name} />
+      ))
+      }
+    </MapContainer >
   );
 }
