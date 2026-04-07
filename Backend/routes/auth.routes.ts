9450bc8b68243f84578e8c38ba3318fdca04868d
Dorfultariant <tunxero@hotmail.com>
2026-04-07 16:06:36 +0300

    Added the password authentication to routes.

--- Backend/routes/auth.routes.ts
+++ Backend/routes/auth.routes.ts
@@ -4,13 +4,17 @@ import {
 	beginRegistration,
 	finishRegistration,
 	beginAuthentication,
-	finishAuthentication
+	finishAuthentication,
+	registerPassword,
+	loginPassword
 } from "../controllers/auth.controller";
 
 const router: Router = Router();
 
 router.post("/create-registration-options", beginRegistration);
 router.post("/create-authentication-options", beginAuthentication);
+router.post("/register-password", registerPassword);
+router.post("/login-password", loginPassword);
 
 router.post("/finish-registration", finishRegistration);
 router.post("/finish-authentication", finishAuthentication);
