2b75f7c78bbe814385e3d2e6412fcd75d2a00bc6
Dorfultariant <tunxero@hotmail.com>
2026-04-09 09:49:34 +0300

    Changed pg to bunjs sql as it is supported

--- Backend/database/helpers.ts
+++ Backend/database/helpers.ts
@@ -1,19 +1,12 @@
 // TODO: Ensure integration with the PostGreatSQL
-import { Pool } from "pg";
+import { SQL } from "bun";
 import type { UserModel, PasskeyModel } from "./models.ts"
 
-// src: https://dev.to/yugjadvani/advanced-integration-connecting-postgresql-with-nodejs-in-a-typescript-ecosystem-3nnh
+// src: https://bun.com/docs/runtime/sql
+const pg = new SQL(
+	process.env.POSTGRES_URL || "postgres://lounastutka:changeme@db:5432/lounastutka");
 
-const pool = new Pool({
-	// connectionString: process.env.DATABASE_URL,
-	user: process.env.POSTGRES_USER,
-	host: process.env.POSTGRES_HOST,
-	database: process.env.POSTGRES_DB,
-	password: process.env.POSTGRES_PASSWORD,
-	port: Number(process.env.POSTGRES_PORT || "5432"),
-});
-
-// Helper for  the 
+// Helper for translating postgres to typescript format for user
 function mapUser(row: Record<string, unknown>): UserModel {
 	return {
 		id: row.id,
@@ -26,68 +19,69 @@ function mapUser(row: Record<string, unknown>): UserModel {
 
 export default {
 	async getUserByEmail(email: string): Promise<UserModel | null> {
-		const res = await pool.query("SELECT * FROM app.users WHERE email=$1", [email]);
-		return res.rows[0] ? mapUser(res.rows[0]) : null;
+		const res = await pg`SELECT * FROM app.users WHERE email=${email}`;
+
+		if (!res || res === undefined) {
+			throw new Error("Database query returned no result");
+		}
+		return res[0] ? mapUser(res[0]) : null;
 	},
 
 	async createUser(email: string): Promise<UserModel> {
-		const res = await pool.query(
-			"INSERT INTO app.users (email) VALUES ($1) RETURNING *",
-			[email]
-		);
-		return mapUser(res.rows[0]);
+		const res = await pg` 
+			INSERT INTO app.users (email) VALUES (${email}) RETURNING *`;
+		if (!res || res === undefined) {
+			throw new Error("Database returned no rows");
+		}
+		return mapUser(res[0]);
 	},
 
 	// NOTE: Support for passwords 
 	async createUserWithPassword(email: string, passwordHash: string): Promise<UserModel> {
-		const res = await pool.query(
-			"INSERT INTO app.users (email, password_hash) VALUES ($1, $2) RETURNING *",
-			[email, passwordHash],
-		);
-		return mapUser(res.rows[0]);
+		const res = await pg`
+			INSERT INTO app.users (email, password_hash) VALUES (${email}, ${passwordHash}) RETURNING *`;
+		if (!res || res === undefined) {
+			throw new Error("Database returned no rows");
+		}
+		return mapUser(res[0]);
 	},
 
 	async updateUserPassword(userId: number, passwordHash: string): Promise<void> {
-		await pool.query(
-			"UPDATE app.users SET password_hash=$1 WHERE id=$2",
-			[passwordHash, userId],
-		);
+		await pg`
+			UPDATE app.users SET password_hash = ${passwordHash} WHERE id = ${userId}
+		`;
 	},
 
 	// NOTE: the challenge is the relevant data to be stored for authentication
 	async storeChallenge(userId: number, challenge: string): Promise<void> {
-		await pool.query(
-			`
-			INSERT INTO app.auth_challenges (challenge, fk_uid)
-			VALUES ($1, $2)
-			ON CONFLICT  (challenge) DO UPDATE SET fk_uid = EXCLUDED.fk_uid, created_at = NOW()
-			`,
-			[challenge, userId]
-		);
+		await pg`
+			INSERT INTO app.auth_challenges(challenge, fk_uid)
+			VALUES(${challenge}, ${userId})
+			ON CONFLICT(challenge) DO UPDATE SET fk_uid = EXCLUDED.fk_uid, created_at = NOW()
+			`;
 	},
 
 	async removeChallenge(userId: number, challenge: string): Promise<boolean> {
-		const res = await pool.query(
-			`
+		const res = await pg`
 			DELETE FROM app.auth_challenges
-			WHERE challenge = $1
-			AND ($2::INTEGER IS NULL OR fk_uid = $2 OR fk_uid IS NULL)
-			`,
-			[challenge, userId],
-		);
+			WHERE challenge = ${challenge}
+			AND fk_uid = ${userId}
+		`;
 		// Can check that remove was success or failure in caller
 		// if no deletes were made, should return false as zero rows DELETE'd
-		if (res.rowCount == null) return false;
-		return (res.rowCount > 0);
+		if (!res || res === undefined) return false;
+		return (res.count > 0);
 	},
 
 	async getChallenge(userId: number) {
-		const res = await pool.query(
-			"SELECT challenge FROM auth_challenges WHERE fk_uid=$1",
-			[userId]
-		);
+		const res = await pg`
+			SELECT challenge FROM app.auth_challenges WHERE fk_uid = ${userId}`;
+		if (!res || res === undefined || !Array.isArray(res)) {
+			throw new Error("Database result was undefined");
+		}
+		if (res.length === 0) return undefined;
 		// Kind of assume only one result with specific ID (which should be viable)
-		return res.rows[0].challenge;
+		return res[0].challenge;
 	},
 
 	async storePasskey(passkey: {
@@ -109,39 +103,33 @@ export default {
 			transports,
 		} = passkey;
 
-		await pool.query(
-			`
-            INSERT INTO app.passkeys (
-                id,
-                public_key,
-                fk_uid,
-                counter,
-                device_type,
-                backed_up,
-                transports
-            )
-            VALUES ($1, $2, $3, $4, $5, $6, $7)
-            `,
-			[
-				credentialId,
-				publicKey,
-				userId,
+		await pg`
+            INSERT INTO app.passkeys(
+				id,
+				public_key,
+				fk_uid,
 				counter,
-				deviceType,
-				backedUp,
-				// Store csv for potential future use
-				transports ? transports.join(",") : null,
-			]
-		);
+				device_type,
+				backed_up,
+				transports
+			)
+		VALUES(
+			${credentialId},
+			${publicKey}, 
+			${userId}, 
+			${counter}, 
+			${deviceType}, 
+			${backedUp}, 
+			${(transports ?? []).join(",")})
+			`;
 	},
 
 	async getPasskeys(userId: number): Promise<PasskeyModel[]> {
-		const res = await pool.query(
-			"SELECT * FROM app.passkeys WHERE fk_uid=$1",
-			[userId]
-		);
+		const res = await pg`
+			SELECT * FROM app.passkeys WHERE fk_uid = ${userId}`;
+		if (!res || res === undefined) throw new Error("Database result was undefined");
 		// Give all viable 
-		return res.rows.map((row) => ({
+		return res.map((row: any) => ({
 			id: row.id,
 			publicKey: new Uint8Array(row.public_key),
 			user: { id: row.user_id } as UserModel,
@@ -154,10 +142,8 @@ export default {
 	},
 
 	async updateCounter(pkId: string, counter: number): Promise<void> {
-		await pool.query(
-			"UPDATE app.passkeys SET counter=$1 WHERE id=$2",
-			[counter, pkId]
-		);
+		await pg`
+			UPDATE app.passkeys SET counter = ${counter} WHERE id = ${pkId} `;
 	},
 };
 
