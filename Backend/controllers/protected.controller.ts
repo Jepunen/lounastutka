4796513b64561df1c90c954baeed2b4845208583
Dorfultariant <tunxero@hotmail.com>
2026-04-18 13:39:15 +0300

    Protected route service skeletons for microservice access.

--- Backend/controllers/protected.controller.ts
+++ Backend/controllers/protected.controller.ts
@@ -1,8 +1,39 @@
-import type { Response } from "express";
+import type { Response, NextFunction } from "express";
 import type { AuthenticatedRequest } from "../middleware/auth.middleware";
+import { AppError } from "../utils/error.ts";
+import {
+  parseWebsiteBasedOnURL,
+  addRestaurantInformationToDatabase
+} from "../services/protected.service.ts";
 
-
+// Test route for authentication testing
 export async function helloThere(req: AuthenticatedRequest, res: Response) {
-	res.status(200).json({ msg: "Hello There but from protected!" });
+  res.status(200).json({ msg: "Hello There but from protected!" });
+}
+
+export async function parseFromSite(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  // TODO: We need to somehow parse the url against injections to the microservice.
+  try {
+    const { restaurantUrl } = req.body;
+    // Pass the url to the microservice.
+    const res = await parseWebsiteBasedOnURL(restaurantUrl);
+
+    // if (!res.ok) {
+    //   throw new AppError("Could not find site.", 400);
+    // }
+  } catch (error: unknown) {
+    next(error);
+  }
+  res.status(200).json({ msg: "Site parsed and added to the database." });
+}
+
+export async function addRestaurantInfo(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  // TODO: Again, we should parse the req against malicious actions.
+  try {
+    const { email } = req.body;
+  } catch (error: unknown) {
+    next(error);
+  }
+  res.status(200).json({ msg: "Restaurant information added successfully." });
 }
 
---
+++ Backend/services/protected.service.ts
@@ -0,0 +1,39 @@
+// Global error class
+import { AppError } from "../utils/error.ts";
+
+import db from "../database/helpers";
+
+// Call the Microservice for parsing the website based on the given URL
+// then store the information to the database if its valid.
+export async function parseWebsiteBasedOnURL(restaurantUrl: string) {
+  const microserviceUrl = process.env.MICROSERVICE_URL ?? "http://microservice/scrape";
+  const res = await fetch(microserviceUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(
+      {
+        urls: restaurantUrl
+      }),
+  });
+
+  if (!res) throw new AppError("Site parsing failed", 500);
+  const data = await res.json();
+  if (!data) throw new AppError("Data could not be parsed", 400);
+  // TODO: Store the data to database
+  // const dbRes = await db.addRestaurantData(tsFormattedData);
+  // WARNING: unsure what to return at this point from the microservice.
+  return true;
+}
+
+// TODO: Database schema based typescript model here
+export async function addRestaurantInformationToDatabase({ }) {
+  // TODO: Validate data from frontend...
+  //
+  // const res = await db.addRestaurantData(tsFormattedData);
+
+  // WARNING: unsure what to return yet.
+  return true;
+}
+
