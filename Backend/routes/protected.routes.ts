afce7a23f1b3fa0438922cce6c84702d960e9504
Dorfultariant <tunxero@hotmail.com>
2026-04-18 13:39:15 +0300

    Protected route service skeletons for microservice access.

--- Backend/routes/protected.routes.ts
+++ Backend/routes/protected.routes.ts
@@ -1,10 +1,14 @@
 import { authenticateToken } from "../middleware/auth.middleware.ts";
-import { helloThere } from "../controllers/protected.controller.ts";
+import { helloThere, parseFromSite, addRestaurantInfo } from "../controllers/protected.controller.ts";
 import { Router } from "express";
-
 
 const router: Router = Router();
 
-router.post("/hello-there", authenticateToken, helloThere);
+// Test route for authentication
+router.get("/hello-there", authenticateToken, helloThere);
+
+// Restaurant addition, information urls for authenticated users.
+router.post("/parse-from-site", authenticateToken, parseFromSite);
+router.post("/add-restaurant-information", authenticateToken, addRestaurantInfo);
 
 export default router;
